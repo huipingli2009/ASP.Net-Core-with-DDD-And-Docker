1e2af09a6a4f86f33fb4064508afbef100de6778
henryli2009 <40966482+huipingli2009@users.noreply.github.com>
2025-02-15 16:58:32 -0500

    EFCore Setup

---
+++ Practice Project with ASP.Net Core with DDD/Persistence/MovieDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Practice_Project_with_ASP.Net_Core_with_DDD.Models;
+
+namespace Practice_Project_with_ASP.Net_Core_with_DDD.Persistence
+{
+	public class MovieDbContext(DbContextOptions<MovieDbContext> options) : DbContext(options)
+	{
+		public DbSet<Movie> Movies => Set<Movie>();
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.HasDefaultSchema("app");
+			modelBuilder.ApplyConfigurationsFromAssembly(typeof(MovieDbContext).Assembly);
+			base.OnModelCreating(modelBuilder);
+		}
+	}
+}
