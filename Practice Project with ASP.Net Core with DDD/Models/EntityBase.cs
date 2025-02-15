fbe674fd1b92dee4e0c8597a7db8f5c35c79f1bd
henryli2009 <40966482+huipingli2009@users.noreply.github.com>
2025-02-15 09:54:34 -0500

    Add project files.

---
+++ Practice Project with ASP.Net Core with DDD/Models/EntityBase.cs
@@ -0,0 +1,14 @@
+﻿namespace Practice_Project_with_ASP.Net_Core_with_DDD.Models
+{
+	public abstract class EntityBase
+	{
+		public Guid Id { get; private init; } = Guid.NewGuid();
+		public DateTimeOffset Created { get; private set; } = DateTimeOffset.UtcNow;
+		public DateTimeOffset LastModified { get; private set; } = DateTimeOffset.UtcNow;
+
+		public void UpdateLastModified()
+		{
+			LastModified = DateTimeOffset.UtcNow;
+		}
+	}
+}
